d37387cc06775ea7a3f26201759e305248db19ba
barkinrl <barkinoral.bo@gmail.com>
2023-07-27 03:08:06 +0300

    app test added and +

--- todo/basemodels.py
+++ todo/basemodels.py
@@ -1,5 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,conint, confloat
 from typing import Optional
+
+class BookQuery(BaseModel):
+    limit: conint(ge=1) = 10
+    offset: conint(ge=0) = 0
+    name: str 
+    author: str 
+    publisher: str 
+    price_min: confloat(ge=0.0) = None
+    price_max: confloat(ge=100.0) = None
 
 class BookResponse(BaseModel):
     id: int
@@ -7,12 +16,14 @@ class BookResponse(BaseModel):
     author: str
     publisher: str 
     price: float
+    message: Optional[str] = "Operation completed successfully."
     
 class BookCreate(BaseModel):
     name: str
     author: str
     publisher: str
     price: float
+    message: Optional[str] = "Operation completed successfully."
 
 
 class IdResponse(BaseModel):
