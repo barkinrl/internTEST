ccab9b98f4ba40b10f31e89c8ff5df7473bb1ebf
barkinrl <44299365+barkinrl@users.noreply.github.com>
2023-07-27 16:09:53 +0300

    Delete todo directory

--- todo/const.py
+++
@@ -1,3 +0,0 @@
-class const:
-    site1 = "https://www.bkmkitap.com/bilim-kurgu-kitaplari"
-    site2 = "https://www.ilknokta.com/index.php?p=Products&q_field_active=0&q=bilimkurgu&search=&q_field=&sort_type=prd_id-desc&page=1"
