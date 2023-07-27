8f3cc18269f2cca0965f127b2f9e2f5e38b83061
barkinrl <barkinoral.bo@gmail.com>
2023-07-27 16:30:50 +0300

    setUp added to test

--- routers/book_router_test.py
+++ routers/book_router_test.py
@@ -1,87 +1,98 @@
-import pytest
+import unittest
 from fastapi.testclient import TestClient
 from routers.book_router import router
- 
-
-client = TestClient(router)
 
 
-# Test data for creating a book
-new_book_data = {
-    "name": "Test Book",
-    "author": "Test Author",
-    "publisher": "Test Publisher",
-    "price": 9.99
-}
+class TestBookAPI(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.client = TestClient(router)
+
+    def setUp(self):
+        # Test data for creating a book
+        self.new_book_data = {
+            "name": "Test Book",
+            "author": "Test Author",
+            "publisher": "Test Publisher",
+            "price": 9.99
+        }
+
+        # Test data for updating a book
+        self.updated_book_data = {
+            "name": "Updated Book",
+            "author": "Updated Author",
+            "publisher": "Updated Publisher",
+            "price": 19.99
+        }
+
+    def test_create_book(self):
+        response = self.client.post("/books", json=self.new_book_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("id", response.json())
+        self.assertEqual(response.json()["name"], self.new_book_data["name"])
+        self.assertEqual(response.json()["author"], self.new_book_data["author"])
+        self.assertEqual(response.json()["publisher"], self.new_book_data["publisher"])
+        self.assertEqual(response.json()["price"], self.new_book_data["price"])
+
+    def test_get_all_books(self):
+        response = self.client.get("/books")
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.json(), list)
+
+    def test_get_book_by_id(self):
+        # Create a new book
+        response = self.client.post("/books", json=self.new_book_data)
+        self.assertEqual(response.status_code, 200)
+        book_id = response.json()["id"]
+
+        # Try to get the book by ID
+        response = self.client.get(f"/books/{book_id}")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["id"], book_id)
+        self.assertEqual(response.json()["name"], self.new_book_data["name"])
+        self.assertEqual(response.json()["author"], self.new_book_data["author"])
+        self.assertEqual(response.json()["publisher"], self.new_book_data["publisher"])
+        self.assertEqual(response.json()["price"], self.new_book_data["price"])
+
+        # Try to get a book with an invalid ID
+        response = self.client.get("/books/9999")
+        self.assertEqual(response.status_code, 404)  # Not Found
+
+    def test_update_book(self):
+        # Create a new book
+        response = self.client.post("/books", json=self.new_book_data)
+        self.assertEqual(response.status_code, 200)
+        book_id = response.json()["id"]
+
+        # Update the book
+        response = self.client.put(f"/books/{book_id}", json=self.updated_book_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["id"], book_id)
+        self.assertEqual(response.json()["name"], self.updated_book_data["name"])
+        self.assertEqual(response.json()["author"], self.updated_book_data["author"])
+        self.assertEqual(response.json()["publisher"], self.updated_book_data["publisher"])
+        self.assertEqual(response.json()["price"], self.updated_book_data["price"])
+
+        # Try to update a book with an invalid ID
+        response = self.client.put("/books/9999", json=self.updated_book_data)
+        self.assertEqual(response.status_code, 404)  # Not Found
+
+    def test_delete_book(self):
+        # Create a new book
+        response = self.client.post("/books", json=self.new_book_data)
+        self.assertEqual(response.status_code, 200)
+        book_id = response.json()["id"]
+
+        # Delete the book
+        response = self.client.delete(f"/books/{book_id}")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["id"], book_id)
+
+        # Try to delete a book with an invalid ID
+        response = self.client.delete("/books/9999")
+        self.assertEqual(response.status_code, 404)  # Not Found
 
 
-# Test data for updating a book
-updated_book_data = {
-    "name": "Updated Book",
-    "author": "Updated Author",
-    "publisher": "Updated Publisher",
-    "price": 19.99
-}
-
-
-# Test case: Create a new book
-def test_create_book():
-    response = client.post("/books", json=new_book_data)
-    assert response.status_code == 200
-    assert "id" in response.json()
-
-
-
-# Test case: Get all books
-def test_get_all_books():
-    response = client.get("/books")
-    assert response.status_code == 200
-    assert isinstance(response.json(), list)
-
-
-
-# Test case: Get a book by ID
-def test_get_book_by_id():
-    # Create a new book
-    response = client.post("/books", json=new_book_data)
-    assert response.status_code == 200
-    book_id = response.json()["id"]
-
-    # Try to get the book by ID
-    response = client.get(f"/books/{book_id}")
-    assert response.status_code == 200
-    assert response.json()["id"] == book_id
-
-
-
-# Test case: Update a book
-def test_update_book():
-    # Create a new book
-    response = client.post("/books", json=new_book_data)
-    assert response.status_code == 200
-    book_id = response.json()["id"]
-
-    # Update the book
-    response = client.put(f"/books/{book_id}", json=updated_book_data)
-    assert response.status_code == 200
-    assert response.json()["id"] == book_id
-
-
-
-
-# Test case: Delete a book
-def test_delete_book():
-    # Create a new book
-    response = client.post("/books", json=new_book_data)
-    assert response.status_code == 200
-    book_id = response.json()["id"]
-
-    # Delete the book
-    response = client.delete(f"/books/{book_id}")
-    assert response.status_code == 200
-    assert response.json()["id"] == book_id 
-
-
-# Run all the test cases
 if __name__ == "__main__":
-    pytest.main()
+    unittest.main()
