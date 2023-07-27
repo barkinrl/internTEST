d33be7b70c00f52046e15a3950a6009e7c6e9f86
barkinrl <barkinoral.bo@gmail.com>
2023-07-27 16:06:35 +0300

    .

---
+++ api_models/basemodels.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel,conint, confloat
+from typing import Optional
+
+class BookQuery(BaseModel):
+    limit: conint = 10
+    offset: conint = 0
+    name: str = None
+    author: str = None
+    publisher: str = None
+    price_min: confloat = None
+    price_max: confloat = None
+
+class BookResponse(BaseModel):
+    id: int
+    name: str
+    author: str
+    publisher: str 
+    price: float
+    message: Optional[str] = "Operation completed successfully."
+    
+class BookCreate(BaseModel):
+    name: str
+    author: str
+    publisher: str
+    price: float
+    message: Optional[str] = "Operation completed successfully."
+
+
+class IdResponse(BaseModel):
+    id:int
+    message: Optional[str] = "Operation completed successfully."
